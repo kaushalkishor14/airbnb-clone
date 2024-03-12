1b52645194333da75da501fc6d28a6caa598deeb
kaushal <kaushalkishorroy21@gmail.com>
2024-03-12 14:21:50 +0530

    register-done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
+import {Routes, Route} from "react-router-dom"
+import IndexPage from "./pages/indexPage";
+import LoginPage from "./pages/LoginPage";
+import OutLet from "./component/OutLet";
+import Register from "./pages/RegisterPage";
+// import { axios } from "axios";
+
+// axios.defaults.baseURL ='http://localhost:5000';
 
 function App() {
- 
-
   return (
-    <div className="bg-red-400">
-      app
-    </div>
-     
-  )
+
+<Routes>
+  <Route path='/' element={<OutLet/>}>
+  <Route path="/" element={<IndexPage/>}/>
+  <Route path="/login" element={<LoginPage/>}/>
+  <Route path="/register" element={<Register/>}/>
+  </Route>
+
+</Routes>
+
+
+
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios"
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser(ev)  {
+    ev.preventDefault();
+    axios.post('http://localhost:5000/register', {
+      name,
+      email,
+      password,
+    });
+  }
+
+
+
+
+
+  return (
+    <div className="mt-4  grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register your Account</h1>
+        <form className="max-w-sm mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@eamil.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button className="primary my-6">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            Already a member?{" "}
+            <Link to={"/login"} className="underline text-black">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}

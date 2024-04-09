1520f955d495dd77ca0e6bcc7afdd826f116c1c5
kaushal <kaushalkishorroy21@gmail.com>
2024-04-09 23:57:55 +0530

    placespage

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,56 +1,9 @@
 /* eslint-disable react/jsx-key */
-import { useState } from "react";
+
 import { Link, useParams } from "react-router-dom";
-import Preks from "../Preks";
-import PhotosUploader from "../../PhotosUploader";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-export default function PlacesPage() {
+export default function PlacesFormPage(){
   const { action } = useParams();
-  const [title, setTitle] = useState("");
-  const [addres, setAddress] = useState("");
-
-  const [description, setDescription] = useState("");
-  const [preks, setPreks] = useState([]);
-  const [extrainfo, setExtraInfo] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [maxGuests, setMaxGuests] = useState(1);
-  const navitate = useNavigate();
-
-  function inputHeader(text) {
-    return <h2 className="text-2xl mt-4 ">{text}</h2>;
-  }
-
-  function inputDescription(text) {
-    return <p className="text-gray-400 text sm">{text}</p>;
-  }
-
-  function preInput(header, description) {
-    return (
-      <>
-        {inputHeader(header)}
-        {inputDescription(description)}
-      </>
-    );
-  }
-
-  async function addNewPlace(ev) {
-    ev.preventDefault();
-    const placeData = {
-      title,
-      addres,
-      description,
-      preks,
-      maxGuests,
-      checkOut,
-      checkIn,
-    };
-    axios.post("/places", placeData);
-    navitate("/places");
-  }
-
   return (
     <div>
       {action !== "new" && (
@@ -78,81 +31,7 @@ export default function PlacesPage() {
         </div>
       )}
       {action === "new" && (
-        <div className="m-auto md:max-w-lg sm:max-w-sm lg:max-w-full">
-          <from onSubmit={addNewPlace}>
-            {preInput("Title", "Title of your place")}
-
-            <input
-              type="text"
-              placeholder="title"
-              value={title}
-              onChange={(ev) => setTitle(ev.target.value)}
-            />
-            {preInput("Addres", "Addres where you vist")}
-
-            <input
-              type="text"
-              value={addres}
-              onChange={(ev) => setAddress(ev.target.valuE)}
-              placeholder="addres"
-            />
-            {preInput("Photos", "Add more photo you like")}
-            <PhotosUploader />
-
-            <h2 className="text-2xl mt-4 ">Description</h2>
-            <p className="text-gray-400 text sm">
-              Describe about your place you vist
-            </p>
-            <textarea
-              value={description}
-              onChange={(ev) => setDescription(ev.target.value)}
-            />
-            <h2 className="text-2xl mt-4 ">Perks</h2>
-            <p className="text-gray-400 text sm">Select all the perks</p>
-            <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-4">
-              <Preks selected={preks} onChange={setPreks} />
-            </div>
-            <h2 className="text-2xl mt-4 ">Extra info</h2>
-            <p className="text-gray-400 text sm">HOuse rules, etc</p>
-            <textarea
-              value={extrainfo}
-              onChange={(ev) => setExtraInfo(ev.target.value)}
-            />
-            <h2 className="text-2xl mt-4 ">Check in&out times </h2>
-            <p className="text-gray-400 text sm">Add check in&out times </p>
-            <div className="grid gap-2 sm:grid-cols-3">
-              <div>
-                <h3 className="mt-2  -mb-1">Check in time</h3>
-                <input
-                  type="text"
-                  value={checkIn}
-                  onChange={(ev) => setCheckIn(ev.target.value)}
-                  placeholder="14:00"
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1">Check out time</h3>
-                <input
-                  type="text"
-                  value={checkOut}
-                  onChange={(ev) => setCheckOut(ev.target.value)}
-                  placeholder="23:00"
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                <input
-                  type="number"
-                  value={maxGuests}
-                  onChange={(ev) => setMaxGuests(ev.target.value)}
-                  placeholder="10"
-                />
-              </div>
-            </div>
-
-            <button className="primary mt-4">Save</button>
-          </from>
-        </div>
+        <PlacesFormPage/>
       )}
     </div>
   );

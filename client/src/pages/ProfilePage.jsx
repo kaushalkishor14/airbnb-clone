4297d1986f65f6c59ebff8261a2829fbc8e082cd
kaushal <kaushalkishorroy21@gmail.com>
2024-04-19 22:01:04 +0530

    mt-4

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -36,7 +36,7 @@ export default function ProfilePage() {
       {subpage === 'profile' && (
         <div className="text-center max-w-lg mx-auto">
           Logged in as {user.name} ({user.email})<br />
-          <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+          <button onClick={logout} className="primary max-w-sm mt-4">Logout</button>
         </div>
       )}
       {subpage === 'places' && (
